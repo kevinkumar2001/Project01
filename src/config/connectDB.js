c0c0ffe17b5f4be6e4abd07bdfdc4daaf87cd9af
kevinkumar2001 <kevinkum.ar0a2@gmail.com>
2024-12-27 08:32:52 +0530

    Update connectDB.js

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-    host: 'localhost',
-    user: 'cluu',
-    password: 'cluu',
-    database: 'cluu',
+    host: 'mysql-38811778-kevinkumar0a2-f691.b.aivencloud.com',
+    user: 'avnadmin',
+    port: 22755,
+    password: 'AVNS_hEoA58j1X-hZdhi1KSy',
+    database: 'defaultdb',
 });
 
 export default connection;
